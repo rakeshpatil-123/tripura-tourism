8ecda73fdb2a67b606f4ca499178e89ab604e45e
joydeep808 <joydeepdebnath868@gmail.com>
2025-07-04 13:39:00 +0530

    fix bugs and add animation counter class

--- src/app/page-content-with-menu/dashboard/stats-card/stats-card.component.ts
+++ src/app/page-content-with-menu/dashboard/stats-card/stats-card.component.ts
@@ -1,38 +1,39 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AnimationCounterService } from '../../../shared/utils/animated-counter';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-stats-card',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './stats-card.component.html',
-  styleUrls: ['./stats-card.component.scss']
+  styleUrls: ['./stats-card.component.scss'],
 })
 export class StatsCardComponent implements OnInit {
   @Input() title: string = 'Full';
-  @Input() value: string = "50";
+  @Input() value: string = '50';
   @Input() type: 'total' | 'approved' | 'pending' | 'rejected' = 'total';
   @Input() iconClass: string = 'fas fa-file-alt';
 
+  constructor(private animationService: AnimationCounterService) {}
   animatedValue: number = 0;
+  private animationSubscription: Subscription | undefined;
   private duration: number = 1000; // Animation duration in ms
 
   ngOnInit(): void {
-    const target = parseInt(this.value, 10);
-    const startTime = performance.now();
+    this.startAnimation(0, parseInt(this.value, 10), this.duration);
+  }
 
-    const animate = (currentTime: number) => {
-      const elapsed = currentTime - startTime;
-      const progress = Math.min(elapsed / this.duration, 1);
-      this.animatedValue = Math.floor(progress * target);
+  startAnimation(start: number, target: number, duration: number): void {
+    if (this.animationSubscription) {
+      this.animationSubscription.unsubscribe();
+    }
 
-      if (progress < 1) {
-        requestAnimationFrame(animate);
-      } else {
-        this.animatedValue = target;
-      }
-    };
-
-    requestAnimationFrame(animate);
+    this.animationSubscription = this.animationService
+      .animateValue(start, target, duration)
+      .subscribe((value) => {
+        this.animatedValue = value;
+      });
   }
 }
