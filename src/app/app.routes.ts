6a671182e62653d79bf912b31fa332156750f270
joydeep808 <joydeepdebnath868@gmail.com>
2025-07-11 17:41:07 +0530

    login page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,9 @@ export const routes: Routes = [
             import('./page-content-with-menu/page-content-with-menu.routes')
                 .then(m => m.PAGE_CONTENT_WITH_MENU_ROUTES)
     },
+    {
+        path: 'auth',
+        loadChildren: () =>
+            import('./page-content/page-content.routes').then(m => m.PAGE_CONTENT_ROUTES)
+    }
 ];
